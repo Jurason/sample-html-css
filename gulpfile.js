bdfaf738a4d05fef87781b70baacd88433139a9b
Eugene <evgenilymanyuk@gmail.com>
2022-09-09 00:48:07 +0300

    update structure

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,17 @@ const browserSync = require("browser-sync");
 
 
 function style() {
-    return gulp.src('./page3/**/*.scss').pipe(sass()).pipe(gulp.dest('./page3/css'))
+    return gulp.src('./page3/**/*.scss').pipe(sass()).pipe(gulp.dest('./page3/css')).pipe(browserSync.stream())
+}
+function watch() {
+    browserSync.init({
+        server: {
+            baseDir: './page3/'
+        }
+    })
+    gulp.watch('./page3/**/*.scss', style)
+    gulp.watch('./page3/*.html').on('change', browserSync.reload)
 }
 
 exports.style = style
+exports.watch = watch
